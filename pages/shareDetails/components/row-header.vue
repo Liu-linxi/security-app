0404ab6c822f408d1063c7ad7f5c92699c1fe1d3
Linxi <1595818766@qq.com>
2024-04-06 22:33:23 +0800

    详情页

---
+++ pages/shareDetails/components/row-header.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<uni-nav-bar left-icon="left" fixed :border="false" leftWidth="140rpx" rightWidth="140rpx" backgroundColor="#e72f2f" color="#FFFFFF">
+			<view class="center">
+				<u-icon name="play-left-fill" color="#ffffff" size="18"></u-icon>
+				<view class="text-center">
+					<view class="font26">
+						行情行情
+					</view>
+					<view class="font-sm">
+						行情行情
+					</view>
+				</view>
+				<u-icon name="play-right-fill" color="#ffffff" size="18"></u-icon>
+			</view>
+			<block slot="right">
+				<view class="rightbloc">
+					<image src="" class="img" mode=""></image>
+					<image src="" class="img" mode=""></image>
+				</view>
+			</block>
+		</uni-nav-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				active:0
+			};
+		},
+		methods: {
+		}
+	};
+</script>
+
+<style scoped lang="scss">
+.center{
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	color: rgba(255, 255, 255, 1);
+	padding: 0 30rpx;
+	
+}
+.rightbloc{
+	width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		.img{
+			display: inline-block;
+			width: 50rpx;
+			height: 50rpx;
+			background-color: rgba(255, 255, 255, 1);
+		}
+	}
+</style>
---
+++ pages/shareDetails/shareDetails.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="">
+		<row-header />
+		<scroll-view scroll-y :style="{height:`${height}px`}" @scroll="comHeight" :scroll-top="scollTop">
+			<view class="" v-for="li in 100">
+				{{li}}
+			</view>
+		</scroll-view>
+
+	</view>
+</template>
+
+<script>
+	import rowHeader from './components/row-header.vue';
+	export default {
+		components: {
+			rowHeader,
+		},
+		data() {
+			return {
+				scollTop: 0,
+				height: 0
+			};
+		},
+		onLoad() {
+			const info = uni.getWindowInfo();
+			this.height = info.windowHeight - 44;
+		},
+		methods: {
+			comHeight(e) {
+				this.scollTop = e.detail.scrollTop;
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
