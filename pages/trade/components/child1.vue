fe91eb6961bd7578fdee6e72617ed4eea0408e19
linxi <1595818766@qq.com>
2024-04-09 16:46:32 +0800

    新增

--- pages/trade/components/child1.vue
+++ pages/trade/components/child1.vue
@@ -1,8 +1,37 @@
 <template>
 	<view class="child1">
 		<view class="top">
-			<view class="bg-red" v-for="(item,index) in 8" :key="index">
-				{{item}}
+			<view class="left">
+				
+			</view>
+			<view class="right">
+				<view class="row1  d-flex flex-column j-sa">
+					<view class="grid3 text-center text-nowrap" v-for="(item,index) in 5" :key="index">
+						<view class="text-left">
+							卖{{item}}
+						</view>
+						<view >
+							--
+						</view>
+						<view >
+							--
+						</view>
+					</view>
+					 
+				</view>
+				<view class="row2  d-flex flex-column j-sa">
+					<view class="grid3 text-center text-nowrap" v-for="(item,index) in 5" :key="index">
+						<view class="text-left">
+							买{{item}}
+						</view>
+						<view >
+							--
+						</view>
+						<view >
+							--
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -33,6 +62,18 @@
 			@include grid-custom(3fr 2fr);
 			background-color: #FFF;
 			padding: 20rpx;
+			height: 600rpx;
+			background-color: #FFF;
+			.right{
+				border: 1px solid #e7e5e6;
+				.row1{
+					height: 299rpx;
+					border-bottom: 1px solid #e7e5e6;
+				}
+				.row2{
+					height: 300rpx;
+				}
+			}
 		}
 	}
 </style>
