83507b86865448c60f1019f9a8d12990eb3c6145
linxi <1595818766@qq.com>
2024-04-10 15:01:11 +0800

    登录修改

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -15,7 +15,7 @@
 			</view>
 			<view class="border-bottom d-flex a-center py-2" v-show="showCode">
 				<image src="" style="width: 50rpx;height:50rpx;" class="bg-red mr-1" mode=""></image>
-				<uni-easyinput v-model="code"  :inputBorder="false" :clearable="false" placeholder="无动态口令可以不输入"></uni-easyinput>
+				<uni-easyinput v-model="code" :inputBorder="false" :clearable="false" placeholder="无动态口令可以不输入"></uni-easyinput>
 			</view>
 		</view>
 		<view class="d-flex px-5 font-sm gray a-center j-sb my-3">
@@ -23,13 +23,13 @@
 				<image src="" style="width: 50rpx;height:50rpx;" class="bg-red mr-1" mode=""></image>
 				增强安全通道
 			</view>
-			
+
 			<view class="blue">
 				证书管理
 			</view>
 		</view>
 		<view class="d-flex j-end">
-			<label class="gray" @click="showCode=!showCode" style="transform: scale(0.7);" >
+			<label class="gray" @click="showCode=!showCode" style="transform: scale(0.7);">
 				动态口令
 				<switch :checked="showCode" class="ml-1" />
 			</label>
@@ -40,9 +40,18 @@
 		<view class="d-flex a-center j-sb mt-3 blue mx-5">
 			登录遇到问题
 			<view class="">
-				保持在线：<text class="border blueBord">10分钟</text>
+				保持在线：<text class="border blueBord" @click="show = true">{{colArray[colIndex]}}</text>
 			</view>
 		</view>
+		<view class="fixed">
+			<view class="gray">
+				不是XX证券客户
+			</view>
+			<view class="blue">
+				立即开户
+			</view>
+		</view>
+		<u-picker :show="show" :columns="columns" @confirm="confirm" @cancel="show=false" title="选择时长"></u-picker>
 	</view>
 </template>
 
@@ -52,12 +61,22 @@
 			return {
 				username: "",
 				password: "",
-				code:"",
-				showCode:false
+				code: "",
+				showCode: false,
+				show: false,
+				columns: [
+					['10分钟自动退出', '30分钟自动退出', '60分钟自动退出', '180分钟自动退出']
+				],
+				colIndex: 0,
+				colArray: ["10分钟", "30分钟", "60分钟", "180分钟"]
 			}
 		},
 		methods: {
-
+			confirm({ indexs }) {
+				this.colIndex = indexs[0];
+				this.show = false;
+				console.log(indexs);
+			}
 		}
 	}
 </script>
@@ -67,12 +86,23 @@
 		::v-deep .uni-easyinput__content-input {
 			background-color: #f6f6f6;
 		}
+
 		.blue {
 			color: #498cdb;
 		}
-		.blueBord{
+
+		.blueBord {
 			border-color: #498cdb;
 			padding: 5rpx 10rpx;
+		}
+
+		.fixed {
+			position: fixed;
+			bottom: 40rpx;
+			left: 0;
+			right: 0;
+			text-align: center;
+
 		}
 	}
 </style>
