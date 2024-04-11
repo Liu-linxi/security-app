7898026223187bee574c6e1d627c94a89362a083
Linxi <1595818766@qq.com>
2024-04-11 22:14:19 +0800

    新增

---
+++ pages/account-analysis/components/row6-echarts.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="charts-box">
+		<qiun-data-charts type="radar" :opts="opts" :chartData="chartData" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				chartData: {},
+				//您可以通过修改 config-ucharts.js 文件中下标为 ['radar'] 的节点来配置全局默认参数，如都是默认参数，此处可以不传 opts 。实际应用过程中 opts 只需传入与全局默认参数中不一致的【某一个属性】即可实现同类型的图表显示不同的样式，达到页面简洁的需求。
+				opts: {
+					color: ["#1890FF", "#91CB74", "#FAC858", "#EE6666", "#73C0DE", "#3CA272", "#FC8452", "#9A60B4", "#ea7ccc"],
+					padding: [5, 5, 5, 5],
+					dataLabel: false,
+					dataPointShape: false,
+					enableScroll: false,
+					legend: {
+						show: false,
+					},
+					extra: {
+						tooltip: {
+							showBox: false
+						},
+						radar: {
+							gridType: "radar",
+							gridColor: "#fce2e4",
+							gridCount: 3,
+							opacity: 1,
+							border: true,
+						}
+					}
+				}
+			};
+		},
+		mounted() {
+			this.getServerData();
+		},
+		methods: {
+			getServerData() {
+				//模拟从服务器获取数据时的延时
+				setTimeout(() => {
+					//模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
+					let res = {
+						categories: ["盈利能力", "选股能力", "交易纪律", "风控能力", "择时能力"],
+						series: [{
+								name: "盈利能力",
+								data: [90, 110, 165, 195, 187],
+							},
+							{
+								name: "选股能力",
+								data: [190, 210, 105, 35, 27]
+							},
+							{
+								name: "选股能力",
+								data: [65, 210, 105, 35, 27]
+							},
+							{
+								name: "选股能力",
+								data: [32, 210, 105, 35, 27]
+							},
+							{
+								name: "选股能力",
+								data: [200, 123, 45, 35, 27]
+							}
+
+						]
+					};
+					this.chartData = JSON.parse(JSON.stringify(res));
+				}, 500);
+			},
+		}
+	};
+</script>
+
+<style scoped>
+	/* 请根据实际需求修改父元素尺寸，组件自动识别宽高 */
+	.charts-box {
+		width: 100%;
+		height: 300px;
+	}
+</style>
--- pages/account-analysis/components/row6.vue
+++ pages/account-analysis/components/row6.vue
@@ -1,29 +1,77 @@
 <template>
-	<view class="row6 mt-2">
-		
+	<view class="row6 mt-2 bg-white">
+		<view class="d-flex a-center j-sb px-3 py-2">
+			<view class=" font36">
+				我的投资能力<uni-icons type="info" size="20" class="ml-1"></uni-icons>
+			</view>
+			<view class="gray">
+				详情
+			</view>
+		</view>
+		<view class="text-center mt-2">
+			<image src="" style="width: 300rpx;height: 340rpx;" class="bg-red" mode=""></image>
+		</view>
+		<view class="flex">
+			<view class="block">
+				<view class="gray">我的表现</view>
+				<view class="d-flex j-sa mt-1">
+					<u-rate  allowHalf v-model="value" size="14" inactiveColor="#FA3534"></u-rate>
+				</view>
+			</view>
+			<image src="" style="width: 80rpx;height: 80rpx;" class="bg-red mx-2" mode=""></image>
+			<view class="block">
+				<view class="gray">全民股民</view>
+				<view class="d-flex j-sa mt-1">
+					<u-rate allowHalf v-model="value" size="14" activeColor="#6381f7" inactiveColor="#6381f7"></u-rate>
+				</view>
+			</view>
+		</view>
+		<row6-echarts />
+
 	</view>
 </template>
 
 <script>
+	import row6Echarts from './row6-echarts.vue';
 	export default {
-		props:{
-			
+		components: {
+			row6Echarts
 		},
-		data(){
-			return{
-				
+		props: {
+
+		},
+		data() {
+			return {
+				value: 3.5
 			}
 		},
 		mounted() {
-			
+
 		},
-		methods:{
-			
+		methods: {
+
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.row6{
+	.row6 {
+		.flex {
+			display: flex;
+			align-items: center;
+			margin: 20rpx 60rpx;
+
+			.block {
+				padding: 14rpx 0;
+				width: 270rpx;
+				background-color: #ececec;
+				border-radius: 8rpx;
+				text-align: center;
+
+				.gray {
+					color: #7d7d7d;
+				}
+			}
+		}
 	}
 </style>
