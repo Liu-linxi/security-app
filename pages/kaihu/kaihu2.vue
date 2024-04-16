95b804f7ab1d9790c7bedb08538de8411d00dee5
linxi <1595818766@qq.com>
2024-04-16 17:24:23 +0800

    新增

--- pages/kaihu/kaihu2.vue
+++ pages/kaihu/kaihu2.vue
@@ -11,12 +11,23 @@
 			</block>
 		</uni-nav-bar>
 		
-		<view class="d-flex row1">
-			<view class="flex-shrink">
-				
-			</view>
-			<view class="flex-shrink">
-				
+		<view class="bg-white p-3">
+			<view class="d-flex row1">
+				<view class="flex-shrink left"></view>
+				<view class="flex-shrink right">
+					<view class="">
+						开户申请已提交成功
+						<view class="gray font-sm">
+							2024-04-02 16:05:11
+						</view>
+					</view>
+					<view class="mt-2">
+						开户申请已提交成功
+						<view class="gray font-sm">
+							2024-04-02 16:05:11
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
 		
@@ -44,6 +55,16 @@
 
 <style lang="scss" scoped>
 	.kaihu2 {
+		.row1{
+			.left{
+				margin: 20rpx 0;
+				width: 2rpx;
+				background-color: #f2f2f2;
+			}
+			.right{
+				margin-left: 60rpx;
+			}
+		}
 		.center {
 			width: 100%;
 			display: flex;
